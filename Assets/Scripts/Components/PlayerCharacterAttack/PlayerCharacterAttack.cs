1429b1ca372c293c99d8a0120ce3c93486e7f2a2
0baka0 <silvelval@naver.com>
2021-03-08 16:59:03 +0900

    Revert "Revert "UnityVirusBlocker""
    This reverts commit 8d4ada840633573549e93fd023c600fa256cb5c3.

--- Assets/Scripts/Components/PlayerCharacterAttack/PlayerCharacterAttack.cs
+++ Assets/Scripts/Components/PlayerCharacterAttack/PlayerCharacterAttack.cs
@@ -34,8 +34,6 @@ public sealed class PlayerCharacterAttack : MonoBehaviour
     {
         PlayerMissile CreateMissileObject() => Instantiate(_PlayerMissilePrefab);
 
-
-
         // 공격용 조이스틱을 얻습니다.
         var attackJoystick = (_PlayerableCharacter.playerController as PlayerController).attackJoystick;
 
@@ -47,9 +45,7 @@ public sealed class PlayerCharacterAttack : MonoBehaviour
 
         newLeftPlayerMissile.Fire(_MissileFireLeftPos.position,
             _MissileFireLeftPos.forward, 10);
-        newLeftPlayerMissile.Fire(_MissileFireRightPos.position,
-            _MissileFireLeftPos.forward, 10);
+        newRightPlayerMissile.Fire(_MissileFireRightPos.position,
+            _MissileFireRightPos.forward, 10);
     }
-
-
 }
